643b363e0cccb80dd640736da25cd50335b35dff
Michael D. Cobian <mdcobian@fluffy.unx.cpp.edu>
2015-11-09 19:01:54 -0800

    Added a third test.

--- Test.java
+++ Test.java
@@ -1,6 +1,7 @@
 public class Test {
 	public static void main(String[] args){
 		System.out.println("Hello World");
+		System.out.println("Extra test number #3.");
 		System.out.println("Goodbye now.");
 	}
 }
